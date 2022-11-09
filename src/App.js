0499f45d2e3914988810d25817e168105e5b4bdd
yogesh-haryana <personalyogesh352@gmail.com>
2022-11-09 12:57:35 +0530

    lint issue solved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import ClassCounter from './Compoents/ClassCounter';
-import HooksCounter from './Compoents/HooksCounter';
-import HookCounterTwo from './Compoents/HookCounterTwo';
-import HooksWithObjects from './Compoents/HooksWithObjects';
-import HooksWithArrays from './Compoents/HooksWithArrays';
-import HooksCounterTitleUpdate from './Compoents/HooksCounterTitleUpdate';
-import MouseMove from './Compoents/MouseMove';
-import MouseContainer from './Compoents/MouseContainer';
-import TickComponent from './Compoents/TickComponent';
-import GetSinglePost from './Compoents/GetSinglePost';
-import Counter from './Compoents/useReducer';
+// import './App.css';
+// import ClassCounter from './Compoents/ClassCounter';
+// import HooksCounter from './Compoents/HooksCounter';
+// import HookCounterTwo from './Compoents/HookCounterTwo';
+// import HooksWithObjects from './Compoents/HooksWithObjects';
+// import HooksWithArrays from './Compoents/HooksWithArrays';
+// import HooksCounterTitleUpdate from './Compoents/HooksCounterTitleUpdate';
+// import MouseMove from './Compoents/MouseMove';
+// import MouseContainer from './Compoents/MouseContainer';
+// import TickComponent from './Compoents/TickComponent';
+// import GetSinglePost from './Compoents/GetSinglePost';
+// import Counter from './Compoents/useReducer';
 import CounterTwo from './Compoents/useReducerTwo';
 
 function App() {
